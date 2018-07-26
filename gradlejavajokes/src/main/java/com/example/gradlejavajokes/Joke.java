a0f2cb9c34f5144d7bfde13d2391bb8946fadc70
Piotr Prus <prus.piotr@gmail.com>
2018-07-26 15:27:52 +0200

    step 1 completed

---
+++ gradlejavajokes/src/main/java/com/example/gradlejavajokes/Joke.java
@@ -0,0 +1,8 @@
+package com.example.gradlejavajokes;
+
+public class Joke {
+
+    public static String getJoke() {
+        return "Why are frogs always so happy? They eat what ever bugs them";
+    }
+}
