4bb0d67c7c83552d723b4b1bc2d096c89ccf7804
Piotr Prus <prus.piotr@gmail.com>
2018-07-30 22:53:46 +0200

    fix some issues base on review

--- app/src/androidTest/java/EndpointAsyncTaskTest.java
+++ app/src/androidTest/java/EndpointAsyncTaskTest.java
@@ -13,6 +13,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
@@ -24,6 +25,6 @@ public class EndpointAsyncTaskTest {
     @Test
     public void jokeIsDisplayedAfterClickingAButton() {
         onView(withId(R.id.tell_joke_button)).perform(click());
-        onView(withId(R.id.joke_tv)).check(matches(not(withText(""))));
+        onView(withId(R.id.joke_tv)).check(matches(withText(endsWith("."))));
     }
 }
--- gradlejavajokes/src/main/java/com/example/gradlejavajokes/Joke.java
+++ gradlejavajokes/src/main/java/com/example/gradlejavajokes/Joke.java
@@ -1,8 +1,19 @@
 package com.example.gradlejavajokes;
 
+import java.util.Random;
+
 public class Joke {
 
     public static String getJoke() {
-        return "Why are frogs always so happy? They eat what ever bugs them";
+        String[] jokesArray = new String[5];
+        jokesArray[0] = " How many programmers does it take to change a light bulb?\n" +
+                "\n" +
+                "None, that's a hardware problem.";
+        jokesArray[1] = "Why are frogs always so happy?\n\n They eat what ever bugs them.";
+        jokesArray[2] = " I love pressing F5.\n\nIt's so refreshing.";
+        jokesArray[3] = "There are only 10 types of people in the world: \nthose who understand binary, \nand those who don't.";
+        jokesArray[4] = "Why do Python Devs need glasses?\n\n Because they don't C#.";
+
+        return jokesArray[new Random().nextInt(jokesArray.length)];
     }
 }
